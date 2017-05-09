a71ce7fb3bfaa560030c448f9d8265c8d70bfb5c
Matt Lomet <mattlomet@live.com>
2017-05-09 10:19:02 -0400

    first commit

---
+++ ruby.rb
@@ -0,0 +1,67 @@
+# problem 1-
+
+# def only_in_america (string)
+# 	puts string + " only in america!"
+# end
+
+# only_in_america("Shirtless and shoesless in walmart ")
+
+
+#problem 2-
+
+# nums = [43, 1, 5, 230, 124]
+
+# def max_num(array)
+# 	# puts array.max
+# 	puts array.sort.last
+# end
+
+# max_num(nums)
+
+#problem 3-
+
+# def fizz_buzz(amount)
+
+# 	(1..amount).each do |i|
+# 		fizzy = ""
+
+# 		fizzy << "Fizz" if i % 3 == 0
+
+# 		fizzy << "Buzz" if i % 5 == 0
+
+# 		if fizzy == ""
+# 			puts i
+# 		else
+# 			puts fizzy
+# 		end
+# 	end
+
+# end
+
+# fizz_buzz(100)
+
+#problem 4-
+
+
+# def key_value (array)
+# 	empty_array = {}
+
+# 	key = array[0].to_sym
+
+# 	value = array[1]
+
+# 	empty_array[key]=value
+
+# puts empty_array
+
+# end
+
+
+
+
+
+
+
+
+
+
